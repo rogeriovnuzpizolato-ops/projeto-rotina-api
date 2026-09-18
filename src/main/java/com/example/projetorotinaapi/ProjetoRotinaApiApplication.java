abc563b2d06b0029292ef7c1ca349b93575f2e2d
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-09-18 19:55:32 -0300

    primeiro commit. feat: adicao do application.properties

---
+++ src/main/java/com/example/projetorotinaapi/ProjetoRotinaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetorotinaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRotinaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoRotinaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projetorotinaapi/ProjetoRotinaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetorotinaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRotinaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
