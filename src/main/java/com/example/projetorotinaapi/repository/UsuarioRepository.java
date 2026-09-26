6f2adcd08a260937e793339be7c137fa8f28d6d9
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-09-26 15:07:08 -0300

    feat: criada interface UsuarioRepository.java

--- src/main/java/com/example/projetorotinaapi/repository/UsuarioRepository.java
+++ src/main/java/com/example/projetorotinaapi/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package com.example.projetorotinaapi.repository;
 
-public interface UsuarioRepository {
+import com.example.projetorotinaapi.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
 }
